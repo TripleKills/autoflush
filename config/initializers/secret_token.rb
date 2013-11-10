709de60271d75f97d8887f69c4f4d7227b7a0875
qy <yiquwireless@163.com>
2013-11-10 11:30:15 +0800

    first submit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autoflush::Application.config.secret_key_base = 'c1c57ece7c5595e4bccba4b20712fc562e1c0143dc770db085828d2aeea93128aceeae816d275b74104b80b2c33c42f0ea06ef14a836d85ed6144156d5f15cb4'
