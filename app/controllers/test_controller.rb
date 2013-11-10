430bfc84eb78e8896bbab4ab25358558a6a5af28
qy <yiquwireless@163.com>
2013-11-10 12:47:39 +0800

    add visit times test_visit

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,13 @@ class TestController < ApplicationController
 
 	def visit()
 		#g_visit("test_visit")
+		visited1 = Visits.find_by_name("test_visit")
+		if visited1.nil?
+			visit1 = 0
+		else
+			visit1 = visited1.times
+		end
+
 		visited2 = Visits.find_by_name("test_visit2")
 		if visited2.nil?
 			visit2 = 0
@@ -16,9 +23,10 @@ class TestController < ApplicationController
 			visit3 = visited3.times
 		end
 
+		@v1 = "test_visit " + visit1.to_s
 		@v2 = "test_visit2 " + visit2.to_s
 		@v3 = "test_visit3 " + visit3.to_s
-		@localhost = get_local_ip
+		o_visit("test_visit")
 	end
 
 	def visit2()
@@ -30,6 +38,11 @@ class TestController < ApplicationController
 	end
 
 	def g_visit(name)
+		times = o_visit(name)
+		render :json => {'view'=>name, 'visited'=>times}.to_json
+	end
+
+	def o_visit(name)
 		visited = Visits.find_by_name(name)
 		if visited.nil?
 			visited = Visits.create(:name=>name,
@@ -37,19 +50,6 @@ class TestController < ApplicationController
 		end
 		visited.times += 1
 		visited.save
-		render :json => {'view'=>name, 'visited'=>visited.times}.to_json
+		visited.times
 	end
-
-	require 'socket'  
-def get_local_ip    
-  orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true    
-# turn off reverse DNS resolution temporarily    
-   
-  UDPSocket.open do |s|    
-    s.connect '64.233.187.99', 1  #google的ip  
-    s.addr.last    
-  end    
-ensure    
-  Socket.do_not_reverse_lookup = orig    
-end
 end
